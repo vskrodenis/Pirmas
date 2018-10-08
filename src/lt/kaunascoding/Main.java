fe67c380af990a7c4bc25bf3e096f8fa204e6c39
vskrodenis <43965280+vskrodenis@users.noreply.github.com>
2018-10-08 19:48:34 +0300

    Update Main.java

--- src/lt/kaunascoding/Main.java
+++ src/lt/kaunascoding/Main.java
@@ -3,6 +3,8 @@ package lt.kaunascoding;
 public class Main {
 
     public static void main(String[] args) {
+	            System.out.println("belkas");
+
 	    int a ;
 	// write your code here
     }
